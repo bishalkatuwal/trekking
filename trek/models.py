fa70fd38067bf4d7339e71b116c6a340201c7175
bishalkatuwal <bishalkatuwal367@gmail.com>
2024-12-09 00:47:51 +0545

    authentication in review

--- trek/models.py
+++ trek/models.py
@@ -121,6 +121,7 @@ class Trip(models.Model):
     end_date = models.DateTimeField(default=timezone.now)
     available_seats = models.PositiveBigIntegerField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    
 
     def __str__(self):
         return self.title
